ac1858b5a09d8ae1dd72f3b8886a665e23ba12ce
Bryan Jangeesingh <brytech@mit.edu>
2023-08-07 16:13:22 -0400

    Initial Commit

---
+++ AppleCashSplit/AppleCashSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCashSplitApp.swift
+//  AppleCashSplit
+//
+//  Created by Bryan Jangeesingh  on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleCashSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
